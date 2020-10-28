d050d548f42e9ffb6e178ddeabefd50cb47330a1
IanKim <iankim2280@gmail.com>
2020-10-28 16:51:01 -0400

    new update

--- server/server.js
+++ server/server.js
@@ -36,17 +36,6 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  // index.html for all page routes
-  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
